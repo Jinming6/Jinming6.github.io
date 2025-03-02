9c4baeaba46a7252b2f59d236fc670ba109d05af
JinMing6 <response965@163.com>
2025-03-02 15:40:31 +0800

    ✨ feat: 调整UI

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import BlurText from "./components/BlurText";
+
+function App() {
+  const handleAnimationComplete = () => {
+    console.log("Animation completed!");
+  };
+  return (
+    <div className="text-white/95 text-center">
+      <BlurText
+        text="🚧 施 工 中 ， 敬 请 期 待 。"
+        delay={150}
+        animateBy="words"
+        direction="top"
+        onAnimationComplete={handleAnimationComplete}
+        className="text-6xl"
+      />
+    </div>
+  );
+}
+
+export default App;
